434d2baebeb3d1f09ef800202027206300d07348
chenyarong <chenyarong@juwang.cn>
2024-10-22 21:49:55 +0800

    init commit

--- install.sh
+++ install.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-if [ ! -d ~/.config/ ]; then
-    mkdir -p ~/.config
+if [ ! -d "$HOME/.config" ]; then
+    mkdir -p $HOME/.config
 fi
 
-if [ -d ~/.config/vim.d ]; then
-    rm -rf ~/.config/vim.d
+if [ -d "$HOME/.config/vim.d" ]; then
+    rm -rf $HOME/.config/vim.d
 fi
 
-git clone -q https://github.com/cyrnicolase/vimrc.git ~/.config/vim.d
+git clone -q https://github.com/cyrnicolase/vimrc.git $HOME/.config/vim.d
 if [ $? != 0 ]; then
     echo "git clone fail!"
     exit 1
 fi
 
-if [ -f ~/.vimrc ]; then
-    mv ~/.vimrc ~/.vimrc.bak
+if [ -f "$HOME/.vimrc" ]; then
+    mv $HOME/.vimrc $HOME/.vimrc.bak
 fi
-ln -s ~/.config/vim.d/.vimrc ~/.vimrc
-
-if [ -d ~/.vim ]; then
-    mv ~/.vim ~/.vim.bak
+if [ -L "$HOME/.vimrc" ]; then
+    rm $HOME/.vimrc
 fi
-ln -s ~/.config/vim.d/.vim ~/.vim
+ln -s $HOME/.config/vim.d/.vimrc $HOME/.vimrc
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ -d "$HOME/.vim" ]; then
+    mv $HOME/.vim $HOME/.vim.bak
+fi
+if [ -L "$HOME/.vim" ]; then
+    rm $HOME/.vim
+fi
+ln -s $HOME/.config/vim.d/.vim $HOME/.vim
+
+curl -sSfLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "OK!!"
